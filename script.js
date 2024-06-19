a0a67368b9b2523b011ee43b0ccbcf43fcc4d6bb
KillianDmt <droumart.k@gmail.com>
2024-06-19 11:31:55 +0200

    maj id function orange click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const orange = document.getElementById('orange');
 
 orange.addEventListener('click', () => {
     let oldScore = document.getElementById('score');
-    let nivMult = document.getElementById('Multi').classList.contains('nivBonus').innerText;
+    let nivMult = document.getElementById('multiplicateurBonus').classList.contains('niveauBonus').innerText;
 
     let newScore = oldScore + (1 * (2 ** nivMult));
 
